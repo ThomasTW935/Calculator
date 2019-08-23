f4e6098e7b4b4ad1f4f04d46bfa273dab7e7cf64
ThomasTW935 <49426473+ThomasTW935@users.noreply.github.com>
2019-08-23 19:45:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,19 @@ $(document).ready(function(){
     $(".calculator-buttons button").click(function(e){
         const buttonValue = e.target.innerHTML;
         const display = $(".calculator-display input");
-        const charLastIndex = display.val().charAt(display.val().length-1);
-        const charFirstInput = buttonValue
+        const charAtLastIndex = display.val().charAt(display.val().length-1);
+        const charFirstInput = buttonValue;
+        const invalidDoubleInput = "%*-/+.";
+        console.log(charAtLastIndex);
+        console.log(display.val() != "" && invalidDoubleInput.includes(charAtLastIndex));
         if(display.val() == "" && "%*/+-".includes(buttonValue)){
             display.val("");
             return;
-        } 
+        }
+        if("%*/+-.".includes(charAtLastIndex) && "%*/+-.".includes(buttonValue)){
+            return;
+        }
+
         switch(buttonValue){
             case 'C': display.val(""); break;
             case '=': display.val(calculate(display.val())); break;
@@ -20,7 +27,6 @@ $(document).ready(function(){
 var historyCount = 0;
 function calculate(string){
     var answer = eval(string);
-    console.log(answer);
     var pointLastIndex = answer.toString().lastIndexOf(".");
     var difference = answer.toString().length-1 - pointLastIndex;
     if(difference > 5){
@@ -32,7 +38,6 @@ function calculate(string){
     if(historyCount >= 4){
         historyDisplay = string;
     }
-    console.log(historyCount);
     $(".calculator-history span").text(historyDisplay);
     return answer;
 }
